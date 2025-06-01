b037dbcce8e7398b5e49542299e62cb8dad71088
Yousuf <yousuf.mallik79@gmail.com>
2025-06-01 22:42:15 +0530

    fix: update layout and styling for New Year Sale Banner component

--- FrontEnd/src/Components/NewYearSaleBanner/NewYearSaleBanner.jsx
+++ FrontEnd/src/Components/NewYearSaleBanner/NewYearSaleBanner.jsx
@@ -8,11 +8,14 @@ function NewYearSaleBanner() {
     window.location.href = ''; // Replace with your desired URL
   };
   return (
-    <div className='mt-20 mb-20 flex justify-center'>
+    <div className='max-w-6xl mx-auto text-center p-4'>
+      <h1 className="text-2xl font-bold mb-6 cursor-default">Coming Soon!!</h1>   
+    <div className='mx-auto flex justify-center'>
       {/* <Link onClick={handleClick} className='cursor-pointer'> */} 
       {/* uncomment the link tag when the sale is live */}
       <img src={SaleBanner} alt="Sale Banner" className='max-w-full h-auto'/>
       {/* </Link> */}
+    </div>
     </div>
   )
 }
